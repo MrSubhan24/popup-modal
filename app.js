abc2f09712d1d87c6a11b6af9e82c4b17ff93ce0
rizkisubhan243 <cubhancode@gmail.com>
2023-09-16 07:16:59 +0700

    index.html

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
-const closeBtn = document.querySelector(".close-btn");
+const closeBtn = document.querySelector(".btn-close");
 
 modalBtn.addEventListener("click", function () {
   modal.classList.add("open-modal");
